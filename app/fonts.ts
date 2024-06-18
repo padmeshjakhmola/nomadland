725667e6274076f0b7122e137a5dc13094a20a18
Prank Shadow <prankshadow@gmail.com>
2024-06-18 14:31:33 +0530

    Initials done

---
+++ app/fonts.ts
@@ -0,0 +1,47 @@
+import {
+  Inter,
+  Lato,
+  Montserrat,
+  Open_Sans,
+  Roboto,
+  Source_Sans_3,
+  Ubuntu,
+} from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const open_sans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
+
+export const lato = Lato({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const source_sans_3 = Source_Sans_3({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["500", "700"],
+});
