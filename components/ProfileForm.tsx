8e7e40a1ac188a94791bb64856674ef97b4aa285
Prank Shadow <prankshadow@gmail.com>
2024-06-25 00:21:43 +0530

    file_upload_create

--- components/ProfileForm.tsx
+++ components/ProfileForm.tsx
@@ -20,7 +20,7 @@ import { open_sans } from "@/app/fonts";
 import { useToast } from "@/components/ui/use-toast";
 import { UserData } from "@/types";
 
-const MAX_FILE_SIZE = 5000000;
+const MAX_FILE_SIZE = 2000000;
 const ACCEPTED_IMAGE_TYPES = [
   "image/jpeg",
   "image/jpg",
@@ -29,16 +29,15 @@ const ACCEPTED_IMAGE_TYPES = [
 ];
 
 const formSchema = z.object({
-  // image: z
-  //   .any()
-  //   .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
-  //   .refine(
-  //     (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-  //     "Only .jpg, .jpeg, .png and .webp formats are supported."
-  //   ),
+  image: z
+    .any()
+    .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 2MB.`)
+    .refine(
+      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported."
+    ),
   title: z.string().min(2).max(50),
   description: z.string().min(2).max(100),
-  link: z.string().min(2).max(50),
 });
 
 // TODO: user is not creating for the first post time after click home back them comming back to create then it is created..
@@ -46,31 +45,34 @@ const formSchema = z.object({
 function ProfileForm({ userData }: { userData: UserData }) {
   const { toast } = useToast();
 
-  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      // image: "",
+      image: null,
       title: "",
       description: "",
-      link: "",
     },
   });
 
+  const handleFileChange = (e: { target: { files: any[] } }) => {
+    const file = e.target.files[0];
+    form.setValue("image", file);
+    console.log("handleFileChange:", file);
+  };
+
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    const userPosts = {
-      title: values.title,
-      description: values.description,
-      link: values.link,
-      userId: userData.id,
-    };
+    const formData = new FormData();
+    console.log("VALUE_IMAGE:", values.image);
+    formData.append("image", values.image);
+    formData.append("title", values.title);
+    formData.append("description", values.description);
+    formData.append("userId", userData.id.toString());
 
     try {
       const response = await fetch("http://localhost:3001/v1/posts", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userPosts),
+        body: formData,
       });
       const result = await response.json();
       toast({ title: "Post created successfully" });
@@ -86,21 +88,23 @@ function ProfileForm({ userData }: { userData: UserData }) {
         onSubmit={form.handleSubmit(onSubmit)}
         className={`${open_sans.className} space-y-8`}
       >
-        {/* <FormField
+        <FormField
           control={form.control}
           name="image"
           render={({ field }) => (
-            <>
-              <FormItem>
-                <FormLabel htmlFor="picture">Your Click</FormLabel>
-                <FormControl>
-                  <Input id="picture" type="file" className="rounded-lg py-2" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </>
+            <FormItem>
+              <FormLabel htmlFor="picture">Your Click</FormLabel>
+              <FormControl>
+                <Input
+                  type="file"
+                  className="rounded-lg py-2"
+                  onChange={handleFileChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
           )}
-        /> */}
+        />
         <FormField
           control={form.control}
           name="title"
@@ -132,25 +136,6 @@ function ProfileForm({ userData }: { userData: UserData }) {
                     placeholder="Add a detailed description"
                     className="resize-none rounded-lg"
                     {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="link"
-          render={({ field }) => (
-            <>
-              <FormItem>
-                <FormLabel className="font-semibold">Link</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Add a link"
-                    {...field}
-                    className="rounded-lg py-2"
                   />
                 </FormControl>
                 <FormMessage />
