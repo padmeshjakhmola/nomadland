699348fe1273d38a204b11850701412a39adb647
Prank Shadow <prankshadow@gmail.com>
2024-07-01 12:45:33 +0530

    incresed file upload size

--- components/ProfileForm.tsx
+++ components/ProfileForm.tsx
@@ -23,7 +23,7 @@ import { useState } from "react";
 import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
 
-const MAX_FILE_SIZE = 2000000;
+const MAX_FILE_SIZE = 5000000;
 const ACCEPTED_IMAGE_TYPES = [
   "image/jpeg",
   "image/jpg",
@@ -34,7 +34,7 @@ const ACCEPTED_IMAGE_TYPES = [
 const formSchema = z.object({
   image: z
     .any()
-    .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 2MB.`)
+    .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
     .refine(
       (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
       "Only .jpg, .jpeg, .png and .webp formats are supported."
