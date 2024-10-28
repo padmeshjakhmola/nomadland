5ac1240eec25d69a40c58cfbfaa850090935aca9
Padmesh Jakhmola <prankshadow@gmail.com>
2024-10-28 21:42:15 +0530

    temp fix 2

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: `${process.env.AWS_HOST}.s3.amazonaws.com`,
+      },
+      {
+        protocol: "https",
         hostname: "nomadland-project-images.s3.ap-south-1.amazonaws.com",
       },
     ],
